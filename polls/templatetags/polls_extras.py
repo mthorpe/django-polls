11a659ea925e8f8f8e41e46086d5d17514e6790e
Melissa Thielen <melissa.thielen@mwcradio.com>
2012-07-26 15:39:05 -0500

    Removed a template filter and added code directly to templates

--- polls/templatetags/polls_extras.py
+++ polls/templatetags/polls_extras.py
@@ -11,16 +11,3 @@ def percentage(fraction, total):
         return ''
     except ZeroDivisionError:
         return 0
-
-@register.filter
-def next_vote_time(poll_repeat_voting):
-    if poll_repeat_voting == 'Daily':
-        vote_again = 'You can vote again tomorrow.'
-    elif poll_repeat_voting == 'Weekly':
-        vote_again = 'You can vote again next week.'
-    elif poll_repeat_voting == 'Unlimited':
-        vote_again = 'You can vote again anytime.'
-    else:
-        vote_again = ''
-        
-    return vote_again
